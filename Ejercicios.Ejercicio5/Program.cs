ae3cdea2915dff3b1af6b0916a47ad5655d288cd
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 00:32:26 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio5/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    Console.WriteLine("Ingrese lo que quiera, el programa finalizará con ctrl+F");
+ConsoleKeyInfo c;
+while ((c = Console.ReadKey()).Modifiers != ConsoleModifiers.Control
+       || c.Key != ConsoleKey.F) { } //Exit on Ctrl + F
+
